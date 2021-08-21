6fbed65e37d54803de060a5d37da28d8a03a498d
luiseduardovb <le_velasquez@yahoo.com>
2021-08-21 12:24:21 -0500

    Add Server Component & refactor

--- section-five/src/app/app.module.ts
+++ section-five/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
