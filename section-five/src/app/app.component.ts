1a527afe7aa53eefb251e751b55b10828e8f1a4f
luiseduardovb <le_velasquez@yahoo.com>
2021-08-22 12:29:59 -0500

    Add View encapsulation to none

--- section-five/src/app/app.component.ts
+++ section-five/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None, //This would make css files global
 })
 export class AppComponent {
   serverElements = [
