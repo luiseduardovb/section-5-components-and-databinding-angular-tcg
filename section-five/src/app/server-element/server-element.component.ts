52d632bd575d4a830e1c5b2c17fa05250b3f9087
luiseduardovb <le_velasquez@yahoo.com>
2021-08-22 15:51:45 -0500

    Implement OnChanges interface and method

--- section-five/src/app/server-element/server-element.component.ts
+++ section-five/src/app/server-element/server-element.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: { type: string; name: string; content: string };
   constructor() {
     console.log('constructor');
   }
 
+  ngOnChanges() {}
 
   ngOnInit(): void {
     console.log('ngOnInit');
