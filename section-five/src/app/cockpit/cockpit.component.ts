8f456684a1b7f0ffefc354cb3932cc4ae11e10e2
luiseduardovb <le_velasquez@yahoo.com>
2021-08-21 19:00:45 -0500

    Create serverCreated and blueprintCreated properts

--- section-five/src/app/cockpit/cockpit.component.ts
+++ section-five/src/app/cockpit/cockpit.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output() blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
   newServerName = '';
   newServerContent = '';
 
