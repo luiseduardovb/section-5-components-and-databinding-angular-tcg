4c0944bff984aafcd56e153644a13cde01822a32
luiseduardovb <le_velasquez@yahoo.com>
2021-08-22 16:07:47 -0500

    add ndOnChanges hook and property

--- section-five/src/app/server-element/server-element.component.ts
+++ section-five/src/app/server-element/server-element.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,11 +13,16 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+
   constructor() {
     console.log('constructor');
   }
 
-  ngOnChanges() {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
     console.log('ngOnInit');
